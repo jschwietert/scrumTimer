423c9edac0bec56b09e92dbbecc4e8c64cb1cffe
Jonathan Schwietert <jonathan.schwietert@gmail.com>
2014-09-09 20:29:49 -0600

    Initial Commit

---
+++ Scrum Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scrum Timer
+//
+//  Created by Jonathan Schwietert on 9/7/14.
+//  Copyright (c) 2014 Jonathan Schwietert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
